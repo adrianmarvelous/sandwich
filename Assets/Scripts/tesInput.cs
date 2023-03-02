15f763cdab66c39668ac46edb37dfe25b7464ed7
Adrian Marvel <marvel.adrian@yahoo.com>
2023-03-02 09:04:57 +0700

    first commit

---
+++ Assets/Scripts/tesInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tesInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.touchCount>0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "box")
+                {
+                    
+                    Color newColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+                    hit.collider.GetComponent<MeshRenderer>().material.color = newColor;
+                    Debug.Log("tes");
+                    //hit.collider.GetComponent<RotateBox>().ChangeBool();
+                }
+            }
+        }
+    }
+
+}
